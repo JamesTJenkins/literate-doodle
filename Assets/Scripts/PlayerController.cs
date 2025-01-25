a41eaf5ef71d7e55903bb69d055886c23930fad8
JamesTJenkins <jenkins01tj@gmail.com>
2025-01-25 11:30:18 +0000

    Basic character controller, prefabbed into scene

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,54 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour {
+	[Header("Camera")]
+	[SerializeField] private Camera playerCamera;
+	[SerializeField] private float sensitivity = 1;
+	[SerializeField] private float maxLookDown = -89f;
+	[SerializeField] private float maxLookUp = 89f;
+	[Header("Movement")]
+	[SerializeField] private float speed = 5f;
+
+	private CharacterController cc;
+	private InputSystem_Actions inputs = null;
+	private float xRotation = 0f;
+
+	private void Start() {
+		inputs = new InputSystem_Actions();
+		inputs.Player.Look.performed += Looking;
+		inputs.Enable();
+
+		cc = GetComponent<CharacterController>();
+
+		Cursor.lockState = CursorLockMode.Locked;
+		Cursor.visible = false;
+	}
+
+	private void OnDestroy() {
+		inputs.Player.Look.performed -= Looking;
+		inputs.Dispose();
+	}
+
+	private void FixedUpdate() {
+		Vector2 input = inputs.Player.Move.ReadValue<Vector2>();
+
+		transform.rotation = Quaternion.Euler(0, playerCamera.transform.eulerAngles.y, 0);
+
+		Vector3 moveVector = new Vector3(input.x, 0f, input.y);
+		Vector3 finalMovement = (transform.right * moveVector.x + transform.forward * moveVector.z) * speed * Time.fixedDeltaTime;
+
+		cc.Move(finalMovement);
+	}
+
+	private void Looking(InputAction.CallbackContext context) {
+		Vector2 mouse = context.ReadValue<Vector2>();
+		mouse *= sensitivity;
+
+		xRotation -= mouse.y;
+		xRotation = Mathf.Clamp(xRotation, maxLookDown, maxLookUp);
+
+		playerCamera.transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+		transform.Rotate(Vector3.up * mouse.x);
+	}
+}
