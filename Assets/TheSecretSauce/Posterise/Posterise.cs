2d24bb80629f7601b67ac8b298a47be144fc2428
JamesTJenkins <jenkins01tj@gmail.com>
2025-01-26 17:08:53 +0000

    Remade posterise shader for hdrp

---
+++ Assets/TheSecretSauce/Posterise/Posterise.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.HighDefinition;
+using System;
+
+[Serializable, VolumeComponentMenu("Post-processing/Custom/Posterise")]
+public sealed class Posterise : CustomPostProcessVolumeComponent, IPostProcessComponent
+{
+    public ClampedIntParameter steps = new ClampedIntParameter(64, 1, 256);
+
+    Material m_Material;
+
+    public bool IsActive() => m_Material != null && steps.value > 0;
+
+    // Do not forget to add this post process in the Custom Post Process Orders list (Project Settings > Graphics > HDRP Global Settings).
+    public override CustomPostProcessInjectionPoint injectionPoint => CustomPostProcessInjectionPoint.AfterPostProcess;
+
+    const string kShaderName = "Shader Graphs/Posterise";
+
+    public override void Setup()
+    {
+        if (Shader.Find(kShaderName) != null)
+            m_Material = new Material(Shader.Find(kShaderName));
+        else
+            Debug.LogError($"Unable to find shader '{kShaderName}'. Post Process Volume Posterise is unable to load. To fix this, please edit the 'kShaderName' constant in Posterise.cs or change the name of your custom post process shader.");
+    }
+
+    public override void Render(CommandBuffer cmd, HDCamera camera, RTHandle source, RTHandle destination)
+    {
+        if (m_Material == null)
+            return;
+
+        m_Material.SetFloat("_Steps", steps.value);
+        HDUtils.DrawFullScreen(cmd, m_Material, destination, shaderPassId: 0);
+    }
+
+    public override void Cleanup()
+    {
+        CoreUtils.Destroy(m_Material);
+    }
+}
