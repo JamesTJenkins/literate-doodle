123dcf149ac194ca3c26b22c2b819d1d587a7496
Andrew Grujic <78950797+AndrewGrujic@users.noreply.github.com>
2025-02-21 11:41:35 +0000

    Added sound events to monster + kill sounds

--- Assets/Scripts/Animation/MonsterSoundEvents.cs
+++ Assets/Scripts/Animation/MonsterSoundEvents.cs
@@ -4,7 +4,25 @@ public class MonsterSoundEvents : MonoBehaviour {
 
 	public AudioSource MonsterWalkSounds;
 
+	public AudioSource HeadTiltSound, MouthOpenSound, KillSound, GoreSound;
+
 	public void StepEvent() {
 		MonsterWalkSounds.Play();
+	}
+
+	public void HeadTiltEvent() {
+		HeadTiltSound.Play();
+	}
+
+	public void MouthOpenEvent() {
+		MouthOpenSound.Play();
+	}
+
+	public void KillSoundEvent() {
+		KillSound.Play();
+	}
+	 
+	public void GoreEvent() {
+		GoreSound.Play();
 	}
 }
